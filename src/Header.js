c84105a9ff2607de2afab72dea003881d1d23c6e
murasakino <120464393+FireDragonGod@users.noreply.github.com>
2023-09-23 15:01:16 +0800

    Create a function to avoid repeating appendChild

--- src/Header.js
+++ src/Header.js
@@ -13,6 +13,12 @@ export class Header {
   a2 = document.createElement('a');
   a3 = document.createElement('a');
   
+  putChildInsideFamilyTree(parent, children) {
+    children.forEach((child) => {
+      parent.appendChild(child);
+    });
+  }
+
   renderHeader() {
     const icon = new Image();
     icon.src = logo;
@@ -26,17 +32,26 @@ export class Header {
     this.a2.setAttribute('id', 'menu');
     this.a3.setAttribute('id', 'contact');
     icon.setAttribute('width', '60');
-    
-    this.head.appendChild(this.h1);
-    this.h1.appendChild(icon);
-    this.head.appendChild(this.nav);
-    this.nav.appendChild(this.ul);
-    this.ul.appendChild(this.li1);
-    this.ul.appendChild(this.li2);
-    this.ul.appendChild(this.li3);
+
+    this.putChildInsideFamilyTree(this.head, [
+      this.h1,
+      this.nav,
+    ]);
+
+    this.putChildInsideFamilyTree(this.h1, [icon]);
+    this.putChildInsideFamilyTree(this.nav, [this.ul]);
+    this.putChildInsideFamilyTree(
+      this.ul, 
+      [
+        this.li1,
+        this.li2,
+        this.li3
+      ]);
+
     this.li1.appendChild(this.a1);
     this.li2.appendChild(this.a2);
     this.li3.appendChild(this.a3);
+
     
     return this.head;
   };
